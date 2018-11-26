ae20444bfb3c15a20dae71d9da01c65c1adbe51c
Tony Man <tonyman316@gmail.com>
2018-11-25 22:44:40 -0800

    show time only, styling

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$b67a9e5825b1dff6$ee59afc8a19af10346de6a8aacc0c362a68c4a33"
+password="pbkdf2(1000,20,sha512)$bb1129e9c8d4853d$f8079c9ce05d666309cde79e9aef2f3f4b09f463"

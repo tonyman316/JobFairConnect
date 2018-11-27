f46bae95c5e93d610e3b1947345d52c2c4448aea
Tony Man <tonyman316@gmail.com>
2018-11-27 11:29:53 -0800

    clean up

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-password="pbkdf2(1000,20,sha512)$a641fe2a27c4e66d$3b831b93562c3034fa5a9a239aede439d0ebf221"
-=======
-password="pbkdf2(1000,20,sha512)$9494d6dd5cdd3d1e$0b3dfa84bbe8f3652c6d7c0bf2e9cfc46b16c1b8"
->>>>>>> student_join_event
+password="pbkdf2(1000,20,sha512)$8f945b5a942acb22$74cb179036a6b214514b03373019ff996a3b5628"
